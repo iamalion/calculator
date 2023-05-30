28cf59a1ce379dec1419a89679900302db4cd1d8
Lindsay Warr <iamalion@gmail.com>
2023-05-30 16:37:45 -0700

    Add new functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,26 @@
 function add(number1,number2){
     return number1 + number2;
 }
-//UI logic
 
+function subtract(number1,number2){
+    return number1 - number2;
+}
+
+function multiply(number1,number2){
+    return number1 * number2;
+}
+
+function divide(number1,number2){
+    return number1 / number2;
+}
+
+//UI logic
 const number1 = parseInt(prompt("Enter a number"));
 const number2 = parseInt(prompt("Enter another number"));
-window.alert(add(number1,number2));
+let result = "those two numbers results in"
+window.alert("Adding " + result + add(number1,number2) + "."); 
+window.alert("Subtracting " + result + subtract(number1, number2) + ".");
+window.alert("Multiplying " + result + multiply(number1,number2)+".");
+window.alert("Dividing " + result + divide(number1,number2)+".");
+;
 
