b80be16e23e402839d6dd7fc2c3885961b8410d4
Lindsay Warr <iamalion@gmail.com>
2023-05-30 16:54:35 -0700

    Change alert message

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,14 @@ function divide(number1,number2){
 //UI logic
 const number1 = parseInt(prompt("Enter a number"));
 const number2 = parseInt(prompt("Enter another number"));
-let result = "those two numbers results in"
-window.alert("Adding " + result + add(number1,number2) + "."); 
-window.alert("Subtracting " + result + subtract(number1, number2) + ".");
-window.alert("Multiplying " + result + multiply(number1,number2)+".");
-window.alert("Dividing " + result + divide(number1,number2)+".");
+window.alert(number1 + "+" + number2 + "=" + add(number1,number2) 
++ number1 + "-" + number2 + "=" + subtract(number1,number2))
+// + number1 + "*" + number2 + "=" + multiply(number1,number2)
+// + number1 + "/" + number2 + "=" + divide(number1,number2))
+// let result = "those two numbers results in "
+// window.alert("Adding " + result + add(number1,number2) + "."); 
+// window.alert("Subtracting " + result + subtract(number1, number2) + ".");
+// window.alert("Multiplying " + result + multiply(number1,number2)+".");
+// window.alert("Dividing " + result + divide(number1,number2)+"."); 
 ;
 
