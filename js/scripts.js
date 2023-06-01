63ddabf440597f0e6827232fd6b516546d597eb3
Lindsay Warr <iamalion@gmail.com>
2023-06-01 16:42:38 -0700

    Continue lesson

--- js/scripts.js
+++ js/scripts.js
@@ -16,16 +16,25 @@ function add(num1, num2) {
     return num1 / num2;
   }
 
+  
+
 //UI logic
 function handleCalculation(event) {
     event.preventDefault();
     const number1 = parseInt(document.querySelector("input#input1").value);
     const number2 = parseInt(document.querySelector("input#input2").value);
-    const operator = document.querySelector("input[name='operator']:checked").value;
-    console.log("Number 1: " + number1);
-    console.log("Number 2: " + number2);
-    console.log("Operator: ", operator);
-    
+    const operator = document.querySelector("input[name='operator']:checked").value;    
+    let result;
+    if (operator === "add") {
+        result = add(number1,number2);
+    } else if (operator === "subtract") {
+        result = subtract(number1,number2);
+    } else if (operator === "multiply") {
+        result = multiply(number1,number2) 
+    } else if (operator === "divide") {
+        result = divide(number1,number2)
+    }
+    document.getElementById("output").innerText = result
 }
 
 window.addEventListener("load", function () {
