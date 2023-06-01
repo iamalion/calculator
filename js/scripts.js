bc1ac8ed9005ee153b02fefab67f11aec3002cb3
Lindsay Warr <iamalion@gmail.com>
2023-06-01 15:27:53 -0700

    Fix bug (typo)

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,34 @@
 
 //business logic
-function add(number1,number2){
-    return number1 + number2;
-}
-
-function subtract(number1,number2){
-    return number1 - number2;
-}
-
-function multiply(number1,number2){
-    return number1 * number2;
-}
-
-function divide(number1,number2){
-    return number1 / number2;
-}
+function add(num1, num2) {
+    return num1 + num2;
+  }
+  
+  function subtract(num1, num2) {
+    return num1 - num2;
+  }
+  
+  function multiply(num1, num2) {
+    return num1 * num2;
+  }
+  
+  function divide(num1, num2) {
+    return num1 / num2;
+  }
 
 //UI logic
-const number1 = parseInt(prompt("Enter a number"));
-const number2 = parseInt(prompt("Enter another number"));
-window.alert(number1 + "+" + number2 + "=" + add(number1,number2)
-+ number1 + "-" + number2 + "=" + subtract(number1,number2) 
-+ number1 + "*" + number2 + "=" + multiply(number1,number2)
-+ number1 + "/" + number2 + "=" + divide(number1,number2))
+function handleCalculation(event) {
+    event.preventDefault();
+    const number1 = parseInt(document.querySelector("input#input1").value);
+    const number2 = parseInt(document.querySelector("input#input2").value);
+    const operator = document.querySelector("input[name='operator']:checked").value;
+    console.log("Number 1: " + number1);
+    console.log("Number 2: " + number2);
+    console.log("Operator: ", operator);
+    
+}
 
-// let result = "those two numbers results in "
-// window.alert("Adding " + result + add(number1,number2) + "."); 
-// window.alert("Subtracting " + result + subtract(number1, number2) + ".");
-// window.alert("Multiplying " + result + multiply(number1,number2)+".");
-// window.alert("Dividing " + result + divide(number1,number2)+"."); 
-;
-
+window.addEventListener("load", function () {
+    const form = document.getElementById("calculator");
+    form.addEventListener("submit", handleCalculation)
+})
